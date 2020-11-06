bc65b4bc909a98919c025852cae733eba4990a1a
cutmeat <johannes.rave@gmail.com>
2020-11-06 11:17:37 +0100

    Initial commit

---
+++ OrientObject_and_code_snippets.js
@@ -0,0 +1,118 @@
+const S = require('Scene');
+const R = require('Reactive');
+const D = require('Diagnostics');
+const DeviceMotion = require('DeviceMotion');
+const Patches = require('Patches');
+
+const numberFormat = 'Time: {0}';
+
+
+// Enter look-at-target and object that needs to look at the target here
+const objectName = 'julesBase';
+const pivotName = 'placer';
+const targetName = 'Camera';
+const trackedPlaneName = 'planeTracker0';
+
+(async function () {
+    D.log("Script activated");
+
+    let [object, pivot, trackedPlane, target] = await Promise.all([
+        S.root.findFirst(objectName),
+        S.root.findFirst(pivotName),
+        S.root.findFirst(trackedPlaneName),
+        S.root.findFirst(targetName)])
+        .catch((error) => D.log(error));
+
+    let deviceTransform = DeviceMotion.worldTransform;
+    let pivotTransform = pivot.worldTransform;
+    let objectWorldTransform = object.worldTransform;
+
+    let lookFromPoint = R.vector(pivotTransform.x, pivotTransform.y, pivotTransform.z);
+    let lookAtPoint = R.point(
+        deviceTransform.x.sub(objectWorldTransform.x),
+        deviceTransform.z.sub(objectWorldTransform.y),
+        deviceTransform.y.sub(objectWorldTransform.z));
+
+    Patches.inputs.setVector('cameraPosition', R.vector(lookAtPoint.x, lookAtPoint.y, lookAtPoint.z));
+    let trackedPlaneRotation = await Patches.outputs.getVector('trackedPlaneRotation');
+    let trackedPlaneFound = await Patches.outputs.getBoolean('trackedPlaneFound');
+
+    object.transform.rotation = R.quaternionFromEuler(0, degToRad(trackedPlaneRotation.z.neg()), 0).mul(deviceTransform.rotation);
+    // object.transform.rotation = deviceTransform.rotation.mul(R.quaternionFromEuler(0, degToRad(trackedPlaneRotation.z), 0));
+    // object.transform.rotation = R.quaternionLookAt(lookAtPoint, R.vector(0,0,1));
+
+    // trackedPlane.
+    // object.transform.position = pivotTransform.position;
+    /*
+    object.transform.rotation = R.quaternionFromEuler(
+        deviceTransform.rotation.eulerAngles.x,
+        deviceTransform.rotation.eulerAngles.y,
+        deviceTransform.rotation.eulerAngles.z,
+    );
+    */
+
+    /*
+    object.transform.rotationZ = deviceTransform.rotationZ.sub(Math.PI/2);
+    object.transform.rotationX = deviceTransform.rotationX.add(1);
+    object.transform.rotationY = deviceTransform.rotationY.mul(-1).sub(pivotTransform.rotationY);
+    */
+
+    // object.transform.rotationX = deviceTransform.rotation.eulerAngles.x;
+    // object.transform.rotationZ = deviceTransform.rotation.eulerAngles.z;
+    // object.transform.rotationY = deviceTransform.rotation.eulerAngles.y
+
+    // object.transform.rotation = pivotTransform.rotation(pivotTransform.rotation, R.vector(0,1,0));
+
+
+
+    let numberFormat = "{:2f}";
+    // let numberFormat = "{:02f}";
+    let debugString = "";
+    debugString = R.concat(debugString, "DeviceMotion (x,y,z):");
+    debugString = debugString.concat("\n");
+    debugString = debugString.concat(radToDeg(deviceTransform.rotation.eulerAngles.x).format(numberFormat));
+    debugString = debugString.concat("\n");
+    debugString = debugString.concat(radToDeg(deviceTransform.rotation.eulerAngles.y).format(numberFormat));
+    debugString = debugString.concat("\n");
+    debugString = debugString.concat(radToDeg(deviceTransform.rotation.eulerAngles.z).format(numberFormat));
+
+    debugString = debugString.concat("\n");
+    debugString = debugString.concat("Tracked plane rotation:");
+    debugString = debugString.concat("\n");
+    debugString = debugString.concat(trackedPlaneRotation.x.format(numberFormat));
+    debugString = debugString.concat("\n");
+    debugString = debugString.concat(trackedPlaneRotation.y.format(numberFormat));
+    debugString = debugString.concat("\n");
+    debugString = debugString.concat(trackedPlaneRotation.z.format(numberFormat));
+
+    debugString = debugString.concat("\n");
+    debugString = debugString.concat("Object rotation:");
+    debugString = debugString.concat("\n");
+    debugString = debugString.concat(radToDeg(object.transform.rotation.eulerAngles.x).format(numberFormat));
+    debugString = debugString.concat("\n");
+    debugString = debugString.concat(radToDeg(object.transform.rotation.eulerAngles.y).format(numberFormat));
+    debugString = debugString.concat("\n");
+    debugString = debugString.concat(radToDeg(object.transform.rotation.eulerAngles.z).format(numberFormat));
+    // debugString = debugString.concat(deviceTransform.rotation.eulerAngles.z.format(numberFormat));
+    // debugString = debugString.concat("\n");
+    // debugString = debugString.concat(deviceTransform.rotation.eulerAngles.x.pinLastValue());
+
+    await Patches.inputs.setString('debugText', debugString);
+
+    D.watch("device x eulerangle:", deviceTransform.rotation.eulerAngles.y);
+
+
+    D.log("End of Log");
+})();
+
+function degToRad(degrees)
+{
+    var pi = Math.PI;
+    return degrees.mul(pi/180);
+}
+
+function radToDeg(radians)
+{
+    var pi = Math.PI;
+    return radians.mul(180/pi);
+}
---
+++ biteScript.js
@@ -0,0 +1,208 @@
+// @ts-nocheck
+const D = require('Diagnostics');
+const S = require('Scene');
+const R = require('Reactive');
+const F = require('FaceTracking');
+const P = require('Patches');
+
+let counter = 0;
+const tolerance = 0.02;
+
+(async function main() {
+    D.log("Script initialized");
+
+    /** @type { SceneObjectBase[] } */
+    const targets = getTargetsFromScene();
+
+    /** @type { SceneObjectBase[] } */
+    const lipMarkers = getLipMarkersFromScene();
+
+    /** @type { PointSignal[] } */
+    const lipPoints = projectFacePointsToFocalPlane(getLipPointsFromFace());
+
+    /** @type { object } */
+    const mouthBounds = findBounds2D(lipPoints);
+
+    /** @type { ScalarSignal } */
+    const mouthOpenness = F.face(0).mouth.openness;
+
+    moveObjectsToPoints(lipMarkers, lipPoints);
+
+    let isMouthOpen = false;
+
+
+
+    mouthOpenness.monitor().subscribe(bite);
+
+    function bite(openness) {
+        if (openness.newValue > 0.4 && isMouthOpen === false) {
+            isMouthOpen = true;
+
+        } else if (openness.newValue < 0.2 && isMouthOpen === true) {
+            isMouthOpen = false;
+            let eatenTargetIndex = indexOfTargetInBounds(targets, mouthBounds, tolerance);
+
+            if (eatenTargetIndex === -1) {
+                D.log('YOU DIDN\'T EAT ANYTHING');
+            } else {
+                counter += 1;
+                P.inputs.setScalar("counter", counter);
+                P.inputs.setPulse("targetEaten" + eatenTargetIndex, R.once());
+                D.log("YOU ATE target" + eatenTargetIndex);
+            }
+        }
+    }
+
+    D.watch("maxX:", mouthBounds.maxX);
+    D.watch("maxY:", mouthBounds.maxY);
+    D.watch("minX:", mouthBounds.minX);
+    D.watch("minY:", mouthBounds.minY);
+    D.watch("mouth openness: ", mouthOpenness);
+
+    D.log("End of Log");
+})()
+
+/** @return { SceneObjectBase[] } */
+async function getTargetsFromScene() {
+    return await Promise.all(
+        [S.root.findFirst("target0"),
+        S.root.findFirst("target1"),
+        S.root.findFirst("target2"),
+        S.root.findFirst("target3"),
+        S.root.findFirst("target4"),
+        S.root.findFirst("target5")]
+    ).catch((error) => D.log(error));
+}
+
+/** @return { SceneObjectBase[] } */
+async function getLipMarkersFromScene() {
+    return await Promise.all([
+        S.root.findFirst("lipTop"),
+        S.root.findFirst("lipBottom"),
+        S.root.findFirst("lipLeft"),
+        S.root.findFirst("lipRight")
+    ]).catch((error) => D.log(error));
+}
+
+/** @return { PointSignal[] }*/
+function getLipPointsFromFace() {
+    return [
+        F.face(0).mouth.upperLipCenter,
+        F.face(0).mouth.lowerLipCenter,
+        F.face(0).mouth.leftCorner,
+        F.face(0).mouth.rightCorner
+    ];
+}
+
+/** @param { PointSignal[] } points
+ *  @return { PointSignal[] } */
+function projectFacePointsToFocalPlane(points) {
+    return points
+        .map(point => F.face(0).cameraTransform.applyToPoint(point))
+        .map(point => S.projectToScreen(point))
+        .map(point => S.unprojectToFocalPlane(point))
+        .map(point => point.neg())
+}
+
+/** @return { object } */
+function findBounds2D(points){
+    if (points.length !== 4){
+        D.log("findBounds2D() requires an array of exactly four points.")
+        return null;
+    }
+    // D.log("Trying to find extrema")
+    return {
+        maxX: findMax('x', points),
+        maxY: findMax('y', points),
+        minX: findMin('x', points),
+        minY: findMin('y', points)
+    }
+}
+
+/** @return { number } */
+function indexOfTargetInBounds(targets, bounds, tolerance) {
+    for (const [i, target] of targets.entries()) {
+        if (isInside(target.transform.position, bounds, tolerance)){
+            D.log("target index: " + i)
+            return i;
+        }
+    }
+    return -1;
+}
+
+/** @return { boolean } */
+function isInside(pos, bounds, tolerance) {
+    return pos.x.pinLastValue() < bounds.maxX.pinLastValue() + tolerance &&
+        pos.x.pinLastValue() > bounds.minX.pinLastValue() - tolerance &&
+        pos.y.pinLastValue() < bounds.maxY.pinLastValue() + tolerance &&
+        pos.y.pinLastValue() > bounds.minY.pinLastValue() - tolerance;
+}
+
+/** @return { ScalarSignal } */
+function findMax(prop, points) {
+    let max = points[0][prop];
+    points.forEach(point => {
+        max = point[prop].max(max)
+    });
+    return max;
+}
+
+/** @return { ScalarSignal } */
+function findMin(prop, points) {
+    let min = points[0][prop];
+    points.forEach(point => {
+        min = point[prop].min(min)
+    });
+    return min;
+}
+
+/**
+ * @param { SceneObjectBase[] } objects
+ * @param { PointSignal[] } points
+ * @return { void } */
+function moveObjectsToPoints(objects, points) {
+    for (const [i, object] of objects.entries()) {
+
+        /** @type { PointSignal } */
+        let point = points[i];
+        object.transform.x = point.x;
+        object.transform.y = point.y;
+        object.transform.z = point.z;
+    }
+}
+
+/*
+const objectName = 'target1';
+const mouthMarkerName = 'plane1';
+
+const [target, mouthMarker3D, mouthCanvas, mouthPos2DMarker] = await Promise.all([
+    S.root.findFirst(objectName),
+    S.root.findFirst(mouthMarkerName),
+    S.root.findFirst("mouthCanvas"),
+    S.root.findFirst("mouthPos")
+]).catch((error) => D.log(error));
+
+// Center of mouth marker:
+let mouthCenterInCameraSpace = F.face(0).cameraTransform.applyToPoint(F.face(0).mouth.center)
+mouthMarker3D.transform.x = mouthCenterInCameraSpace.x;
+mouthMarker3D.transform.y = mouthCenterInCameraSpace.y;
+
+D.watch("mouthX 3D:", mouthMarker3D.transform.x)
+D.watch("mouthY 3D:", mouthMarker3D.transform.y)
+
+// Trying to use canvas-coordinates for game logic:
+
+// Canvas height and width
+const canvasWidth = mouthCanvas.width
+const canvasHeight = mouthCanvas.height
+
+let mouthPos2D = S.projectToScreen(mouthCenterInCameraSpace);
+mouthPos2DMarker.transform.x = mouthPos2D.x.sub(mouthPos2DMarker.width.div(2));
+mouthPos2DMarker.transform.y = mouthPos2D.y.add(mouthPos2DMarker.height).mul(-1).add(canvasHeight);
+// mouthPos2DMarker.transform.y = canvasHeight.sub(mouthPos2D.y).sub(mouthPos2DMarker.height.div(2));
+// D.log(mouthPos2DMarker.transform.x.pinLastValue());
+// D.log(mouthPos2DMarker.transform.y.pinLastValue());
+
+D.watch("mouthX 2D:", mouthPos2D.x)
+D.watch("mouthY 2D:", mouthPos2D.y)
+*/
